3ed578bd7a4af1a80965363f1733d938f2f66b18
shubhharkawat <shubhharkawat1406@gmail.com>
2021-12-06 13:57:18 +0530

    List of Startups page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './Header'
+import SearchBar from './SearchBar'
+import Content from './Content'
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <div className="App__Header">
+          <Header />
+        </div>
+        <Switch>
+          < Route path="/">
+            <div className="App__SearchBar">
+              <SearchBar />
+            </div>
+          </Route>
+          < Route path="/">
+
+          </Route>
+        </Switch>
+        <div className="App__Body">
+          <div className="App__Content"><Content /></div>
+        </div>
+      </Router>
     </div>
   );
 }
